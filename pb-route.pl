74bf80d851bb496c268f481a4280b6c7c1e64d9d
Phillip Smith <fukawi2@gmail.com>
2010-03-04 17:20:02 +1100

    added licensing information

--- pb-route.pl
+++ pb-route.pl
@@ -1,4 +1,20 @@
 #!/usr/bin/perl -w
+
+# Copyright (C) 2010 Phillip Smith
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  
 use strict;
 use warnings;
@@ -36,6 +52,12 @@ my %config; # The hash where the config will be stored once read
 ###############################################################################
 ### Initialize and Setup Standard Configuration
 ###############################################################################
+&comment('');
+&comment('pb-route, Copyright (C) 2010 Phillip Smith');
+&comment('This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are');
+&comment('welcome to use and redistribute it under the conditions of the GPL license version 2');
+&comment('See the "COPYING" file for further details.');
+&comment('');
 &setup_route_tables;
 &ipt_flush;
 &initialize_mangle;
